331e3adacc82271134c208197a51b6d50b498a04
MasumaJaffery <masumajaffery1@gmail.com>
2023-09-22 05:21:53 -0700

    refactor

--- solver_spec.rb
+++ solver_spec.rb
@@ -1,22 +1,26 @@
 require 'rspec'
-require_relative 'solver';
+require_relative 'solver'
 
 describe Solver do
-before each: do
-@solver = Solver.new
-end
-context "Factorial Mehod" do 
-it "Factorial Number Positive" do
-result = @solver.factorial(2)
-expect(result).toeq (2)
-end
-it "Factorial Number Negative" do 
-expect { @solver.factorial(-2) }.to raise_error(ArgumentError, "Factorial is not defined for negative integers.")
-end
-end
-context "Reverse Mehod" do
-it "Reverse a String" do
-result = @solver.reverse("hello")
-expect(result). toeq ("olleh")
-end
+  before :each do
+    @solver = Solver.new
+  end
+
+  context "Factorial Method" do 
+    it "Factorial Number Positive" do
+      result = @solver.factorial(2)
+      expect(result).to eq(2)
+    end
+
+    it "Factorial Number Negative" do 
+      expect { @solver.factorial(-2) }.to raise_error(ArgumentError, "Factorial is not defined for negative integers.")
+    end
+  end
+
+  context "Reverse Method" do
+    it "Reverse a String" do
+      result = @solver.reverse("hello")
+      expect(result).to eq("olleh")
+    end
+  end
 end
