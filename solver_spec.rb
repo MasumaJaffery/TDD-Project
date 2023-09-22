cebe232fb9694e733ecd5aca6dbf43f234f68d62
Najibullah Jafari <121656832+najibullahjafari@users.noreply.github.com>
2023-09-22 17:43:55 +0430

    Add test for devisible by 3 and 5

--- solver_spec.rb
+++ solver_spec.rb
@@ -30,4 +30,26 @@ describe Solver do
     expect(f).to eq('fizz')
   end
 end
+
+  describe 'fizzbuzz' do
+  it "returns 'buzz' for devisible by 5" do
+    f = fizzbuzz(20)
+    expect(f).to eq('buzz')
+  end
+end
+
+describe 'fizzbuzz' do
+  it "returns 'buzzbuzz' for devisible by 5 and 3" do
+    f = fizzbuzz(15)
+    expect(f).to eq('fizzbuzz')
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns a string for a number that is not divisible by 3 and 5' do
+    f = fizzbuzz(26)
+    expect(f).to be_a(String)
+  end
+end
+
 end
